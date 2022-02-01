15f2049cf6721d84d5f5b01f84795bdb8c1f7da8
DarkWooky <mevkysno@gmail.com>
2022-02-01 14:14:23 +0300

    Project created

---
+++ OneScreenApp/OneScreenApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OneScreenApp
+//
+//  Created by Egor Mikhalevich on 1.02.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
